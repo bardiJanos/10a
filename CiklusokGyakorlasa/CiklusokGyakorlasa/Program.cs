d39a3db25ad55cb6e411a4f79b2203877de9a998
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-24 11:08:53 +0200

    Ciklus gyakorlás

---
+++ CiklusokGyakorlasa/CiklusokGyakorlasa/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiklusokGyakorlasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for ciklus
+            //a 2 első 20 hatványát írjuk ki.
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Math.Pow(2,i));
+            }
+
+            //Készítsük el while-al ugyanezt!
+            int hatvany = 8;
+            while (hatvany<=40)
+            {
+                Console.WriteLine(Math.Pow(2, hatvany));
+                hatvany++;
+                //hatvany=hatvany+1; -ezt is írhattam volna
+
+            }
+
+            hatvany = 40;
+            while (hatvany >= 0)
+            {
+                Console.WriteLine(Math.Pow(2, hatvany));
+                hatvany--;
+                //hatvany-=1; így is írhattam volna
+
+            }
+
+            hatvany = 0;
+            do
+            {
+                Console.WriteLine(Math.Pow(2, hatvany));
+                hatvany++;
+
+            } while (hatvany<=40);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CiklusokGyakorlasa/Szambekeres/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szambekeres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+
+            Console.Write("A:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("B:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            //0 esetén újra be kell kérni, mert nem lehet 0-val osztani
+            while (b==0)
+            {
+                Console.Write("Rossz!!B:");
+                b = Convert.ToDouble(Console.ReadLine());
+            }
+
+            c = a / b;
+
+            Console.WriteLine(c);
+
+
+            Console.ReadKey();
+        }
+    }
+}
