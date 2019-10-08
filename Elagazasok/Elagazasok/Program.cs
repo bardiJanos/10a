98d05d3c3e80c836b064241805fba4a62cee1346
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-08 09:56:18 +0200

    Egyszeres szelekciók

---
+++ Elagazasok/Elagazasok/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elagazasok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Elágazások
+            //Egyszeres egyágú
+            int a = -12;
+            if (a>0)
+            {
+                Console.WriteLine("Pozitív szám");
+            }
+
+            //egyszeres, kétágú
+            if (a>=0)
+            {
+                Console.WriteLine("Pozitív szám");
+            }
+            else
+            {
+                Console.WriteLine("Negatív szám");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Elagazasok/Elagazasok/elagazasok.md
@@ -0,0 +1,28 @@
+﻿# Elágazások (eset szétválasztás)
+
+## Elágazások típusai:
+### Egyszeres egyágú
+
+példa 1: Megvizsgáljuk, hogy a értéke pozitív-e.
+```C#
+int a = 12;
+if (a>0)
+{
+    Console.WriteLine("Pozitív szám");
+}
+```
+### Egyszeres kétágú
+
+```C#
+if (a>=0)
+{
+    Console.WriteLine("Pozitív szám");
+}
+else
+{
+    Console.WriteLine("Negatív szám");
+}
+```
+
+### Többszörös
+
