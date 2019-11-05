eaaf51e7c071a471bd0fdfe459bb7cbca0801a9f
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-05 10:36:21 +0100

    MEgszámlálás

--- AlapAlgoritmusok/AlapAlgoritmusok/Program.cs
+++ AlapAlgoritmusok/AlapAlgoritmusok/Program.cs
@@ -8,20 +8,32 @@ namespace AlapAlgoritmusok
 {
     class Program
     {
+        /// <summary>
+        /// Ez a program az összegzés algoritmusát mutatja be
+        /// </summary>
+        /// <param name="args">Paranccsori paraméterek</param>
         static void Main(string[] args)
         {
             //tömb létrehozása
-            int[] szamok = new int[] {1,211,56,97,445,19,49,76 };
+            int[] szamok = new int[] {1,211,56,97,445,199,49,76 };
             //ez a változó gyűjti az összeget
             int osszeg = 0;
             //ebbe a változóba darabszámot gyűjtünk
             int darab = 0;
             
+            //TODO:tennivaló
+
+
             //ciklus, ami annyiszor fut le, ahány elem van a tömbben
             for (int i = 0; i < szamok.Length; i++)
             {
                 osszeg = osszeg + szamok[i];
-                darab = darab + 1;
+
+                if (szamok[i]>100)
+                {
+                    darab = darab + 1;
+                }
+               
             }
 
             Console.WriteLine($"Összeg:{osszeg},darabszám:{darab}");
