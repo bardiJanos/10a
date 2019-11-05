d9b36775b237f0c3fd0acecbb927f1930a989bd0
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-05 10:16:22 +0100

    Összegzés

---
+++ AlapAlgoritmusok/AlapAlgoritmusok/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlapAlgoritmusok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tömb létrehozása
+            int[] szamok = new int[] {1,211,56,97,445,19,49,76 };
+            //ez a változó gyűjti az összeget
+            int osszeg = 0;
+            //ebbe a változóba darabszámot gyűjtünk
+            int darab = 0;
+            
+            //ciklus, ami annyiszor fut le, ahány elem van a tömbben
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                osszeg = osszeg + szamok[i];
+                darab = darab + 1;
+            }
+
+            Console.WriteLine($"Összeg:{osszeg},darabszám:{darab}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
