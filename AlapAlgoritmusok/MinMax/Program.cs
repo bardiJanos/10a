ea76477a5b195beecd1dd93ed08793b468d6cdd4
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-05 11:00:43 +0100

    Minimum és maximum kiválasztása

---
+++ AlapAlgoritmusok/MinMax/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Minimum és maximum meghatározás
+            int[] szamok = new int[] {18,3,79,66,945,411 };
+            //olyan kezdőértéket állítunk be, aminél biztosan lesz
+            //nagyobb
+            int max = Int32.MinValue;
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                if (szamok[i]>max)
+                {
+                    max = szamok[i];
+                }
+            }
+
+            Console.WriteLine($"A maximum:{max}");
+            Console.WriteLine($"Ellenőrzés:{szamok.Max()}");
+
+            Console.ReadKey();
+        }
+    }
+}
