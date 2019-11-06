34094c84e1092ded9780883ea5ce7ed693c046ab
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-06 08:57:41 +0100

    Kezdőérték megadása
    Általánosabb a megoldás, ha a tomb első elemét adjuk meg kezdőértéknek

--- AlapAlgoritmusok/MinMax/Program.cs
+++ AlapAlgoritmusok/MinMax/Program.cs
@@ -14,8 +14,15 @@ namespace MinMax
             int[] szamok = new int[] {18,3,79,-66,945,411 };
             //olyan kezdőértéket állítunk be, aminél biztosan lesz
             //nagyobb
-            int max = Int32.MinValue;
-            int min = Int32.MaxValue;
+            //int max = Int32.MinValue;
+            //int min = Int32.MaxValue;
+            //Kezdőértéknek a tömb első elemét is beállíthatjuk
+            //így egyszerűbb a program
+            int max = szamok[0];
+            int min = szamok[0];
+
+
+            
 
             for (int i = 0; i < szamok.Length; i++)
             {
