60ae91e1de0e32a6f570c4e80f0bcea32b33cd42
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-06 10:54:24 +0100

    Keresés

---
+++ AlapAlgoritmusok/Nevsor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nevsor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A feladat: Az előző program mintájára
+            //írjon programot, ami egy bekért névről
+            //megmondja, hogy szerepel-e a névsorban
+            //A névsor egy tömbben legyen (7-8 név)
+
+
+        }
+    }
+}
