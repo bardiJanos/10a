27d4f4a79ac90191545a35934a49ba6d9ccdf820
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-12 10:01:15 +0100

    Tömb létrehozás, feltöltés értékekkel

---
+++ TombokR/TombokR/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TombokR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alap algoritmusok gyakorlása, véletlenszámok
+            
+            //200 elemű tömb létrehozása
+            int[] szamok = new int[200];
+
+            //Hogyan kéne feltölteni a tömböt véletlen számokkal?
+            //Véletlen szám létrehozás
+            Random rand = new Random();
+
+            //Tömb feltöltése
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = rand.Next(-100, 101);
+            }
+
+            //Tömb elemeinek kiíratása
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine(szamok[i]);
+            }
+
+
+        }
+    }
+}
---
+++ TombokR/TombokR/tombok.md
@@ -0,0 +1,31 @@
+﻿# Tömbök, véletlen számok létrehozása
+
+Az alap algoritmusok gyakorlásához először létrehozunk egy 200 elemű tömböt.
+
+```C#
+int[] szamok = new int[200];
+```
+
+Véletlen számokkal akarjuk feltölteni a tömböt, ehhez inicializálni kell
+a véletlenszám-generátort.
+```C#
+Random rand = new Random();
+```
+A tömb feltöltését ciklussal végezzük:
+
+```C#
+for (int i = 0; i < szamok.Length; i++)
+{
+    szamok[i] = rand.Next(-100, 101);
+}
+```
+
+Az elemek kiíratását szintén ciklussal végezzük:
+
+```C#
+for (int i = 0; i < szamok.Length; i++)
+{
+    Console.WriteLine(szamok[i]);
+}
+```
+
