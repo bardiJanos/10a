634b8ef3cff18520c108008c8f8e99970eb4d65a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-20 09:54:39 +0100

    Lista és összetett adattípus kezelése

---
+++ Listak/Listak/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Listák (dinamikus tömb)
+            //Deklarálás
+            List<int> szamok = new List<int>();
+
+            //elem hozzádása 
+            szamok.Add(122);
+            szamok.Add(212);
+            szamok.Add(33);
+
+            //a lista elemeinek a száma: Count
+            Console.WriteLine(szamok.Count);
+
+            //a lista elemeinek kiírása 
+            for (int i = 0; i < szamok.Count; i++)
+            {
+                Console.WriteLine(szamok[i]);
+            }
+            //A count-ot leszámítva ugyanaz, mint a tömb kiíratása
+
+            //Könnyen megállapítható, hogy egy elem
+            //rajta van-e a listán
+            //Ha az IndexOf -1 et ad, akkor nincsen
+            Console.WriteLine(szamok.IndexOf(299));
+
+            if (szamok.IndexOf(299)==-1)
+            {
+                Console.WriteLine("Ez nincs a listán");
+            }
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Listak/OsszetettTipus/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsszetettTipus
+{
+    class Program
+    {
+        //struktúra, azaz összetett adattípus
+        //létrehozása
+        struct Szemely {
+            public string vezeteknev;
+            public string keresztnev;
+            public int szuletesEve;
+            public int magassag;
+        }
+
+        static void Main(string[] args)
+        {
+            //A listán szereplő elemek Szemely típusúak
+            //A Szemely típust a struct (struktúra) írja le.
+            List<Szemely> szemelyek = new List<Szemely>();
+
+            //Kell egy példány a Személy típusból
+            Szemely valaki = new Szemely();
+
+            //Mostmár lehet értéket adni a valaki-nek
+            valaki.vezeteknev = "Nagy";
+            valaki.keresztnev = "Ubul";
+            valaki.szuletesEve = 2001;
+            valaki.magassag = 188;
+
+            //A valaki-t mostmár hozzá lehet adni a listához
+            szemelyek.Add(valaki);
+
+            valaki.vezeteknev = "Kerekes";
+            valaki.keresztnev = "Eufrozina";
+            valaki.szuletesEve = 1988;
+            valaki.magassag = 168;
+
+            szemelyek.Add(valaki);
+
+            //A lista elemeinek kiíratása
+            for (int i = 0; i < szemelyek.Count; i++)
+            {
+                Console.WriteLine($"Név:{szemelyek[i].vezeteknev}, Keresztnév:{szemelyek[i].keresztnev} ");
+            }
+
+            //A lista elemeinek kiírtása foreach-el
+            foreach (var v in szemelyek)
+            {
+                Console.WriteLine($"Veznév:{v.vezeteknev},{v.keresztnev},{v.magassag},{v.szuletesEve}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
