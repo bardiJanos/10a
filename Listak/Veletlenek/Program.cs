e8d46da69cb23071de54fd31010ba1388130567d
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-20 10:07:55 +0100

    Véletlen elemszámú lista

---
+++ Listak/Veletlenek/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veletlenek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Írjon programot, amely létrehoz egy
+            //véletlen elemszámú (50 és 100 közötti)
+            //listát, és -100 és 200 közötti értékeket
+            //rak bele
+            //Írja ki a program az elemszámot és az elemeket!
+            List<int> vszamok = new List<int>();
+            Random rand = new Random();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
