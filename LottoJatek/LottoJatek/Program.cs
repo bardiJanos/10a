da072d0230b4580fb46e9d3221e29bf01125e3c1
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-12-04 09:59:07 +0100

    Lotto játék változó deklarációk, és tippek bekérése

---
+++ LottoJatek/LottoJatek/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoJatek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hány számmal játszunk?:");
+            int hanySzam = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Hány számból sorsolunk?:");
+            int szamTer = Convert.ToInt32(Console.ReadLine());
+
+            int[] tippek = new int[hanySzam];
+            int[] nyeroSzamok = new int[hanySzam];
+            int talalatok = 0;
+
+            for (int i = 0; i < tippek.Length; i++)
+            {
+                Console.Write($"{i+1}.tipp:");
+                var temp= Convert.ToInt32(Console.ReadLine());
+                while (temp<1 || temp>szamTer || tippek.Contains(temp))
+                {
+                    Console.Write($"Rossz tipp!Újra!{i + 1}.tipp:");
+                    temp = Convert.ToInt32(Console.ReadLine());
+                }
+                tippek[i] = temp;
+            }
+
+            for (int i = 0; i < tippek.Length; i++)
+            {
+                Console.Write(tippek[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LottoJatek/LottoJatek/elkeszites.md
@@ -0,0 +1,43 @@
+﻿# Lottójáték készítése
+
+Minden lottójátékban meg kell adni adott számú tippet.
+Minden lottójátékban megadott számok közül sorsolnak a tippek számával megegyező számú nyerőszámot.
+
+A tippek és a nyerőszámok összehasonlítása alapján lehet meghatározni a találatokat.
+
+**Fontosabb feladatok:**
+
+**adatszerkezetek meghatározása**
+
+tömbök: tippek, nyeroszamok 
+
+elemszámukat a program futásakor határozzuk meg!
+
+**szükséges változók** 
+
+-  hány tippel játszunk
+-  hány számból húzunk 
+-  találatok száma
+
+**1. lépés**
+
+A tippek számának,és a számtérnek a meghatározása.
+
+**2.lépés**
+
+A tippek bekérése
+
+A tippnek a következőknek kell megfelelnie:
+- legyen a megadott határok (1..szamTer) között
+- a tippeknek egyedinek kell lennie (ha már szerepel a tippek között, újra be kell kérni)
+- addig kell a bekérést folytatni, amíg minden tipp egyedi nem lesz.
+
+
+
+
+
+
+
+
+
+
