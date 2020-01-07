4d539be8f16cf2a060187d527a39163a63ffa7d3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-01-07 11:00:57 +0100

    Fájl beolvasás ReadAllLines-al

---
+++ FajlOsszesSor/FajlOsszesSor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FajlOsszesSor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            try
+            {
+                var sorok = File.ReadAllLines(@"tesztadat_20k.txt", Encoding.Default);
+                for (int i = 0; i < sorok.Length; i++)
+                {
+                    Console.WriteLine(sorok[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
