fce16da1a37b1cf4cd5240a8b913225032e5673e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-01-14 10:42:16 +0100

    adatok feldolgozása ciklussal, lekérdezés a létrejött listából

--- AdatFeldolgozas/AdatFeldolgozas/Program.cs
+++ AdatFeldolgozas/AdatFeldolgozas/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,48 @@ namespace AdatFeldolgozas
 
         static void Main(string[] args)
         {
+            //Lista létrehozása
             List<Szemely> szemelyek = new List<Szemely>();
+            //struktúra példány létrehozása
             Szemely szemely = new Szemely();
-            
 
+            try
+            {
+                //Filestream létrehozása a beolvasáshoz
+                FileStream fajl = new FileStream(@"tesztadat_20k.txt", FileMode.Open);
+                //a streamreader olvassa a fájl majd
+                StreamReader reader = new StreamReader(fajl,Encoding.Default);
 
+                while (!reader.EndOfStream)
+                {
+                    //a cikluson belül dolgozzuk fel a fájlból beolvasott sorokat
+                    //a fájlból beolvasott sort a , karakter mentén a split 
+                    //elvágja, a részeket egy tömbbe teszi
+                    var adat = reader.ReadLine().Split(',');
+
+                    //kitöltjük a struktúrapéldány mezőit adatokkal
+                    szemely.vezeteknev = adat[0];
+                    szemely.keresztnev = adat[1];
+                    szemely.szuletesEve = Convert.ToInt32(adat[2]);
+                    szemely.szuletesiHely = adat[3];
+
+                    //a struktúra példányát a listához hozzáadjuk
+
+                    szemelyek.Add(szemely);
+
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                //Ha van kivétel, kiíratjuk az üzenetét
+                Console.WriteLine(ex.Message);
+            }
+
+            //Itt tudunk valamit kezdeni az adatokkal
+            Console.WriteLine($"Adatok száma:{szemelyek.Count}");
+
+            var nyolcvanas = szemelyek.FindAll(x=>x.szuletesEve>1980);
 
             Console.ReadKey();
         }
