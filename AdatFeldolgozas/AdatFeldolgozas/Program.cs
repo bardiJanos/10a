4affff8ad1252094e9e5b297ca8767636b953e86
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-01-14 09:58:15 +0100

    Adatfeldolgozas
    Struktúra és lista deklarálás

---
+++ AdatFeldolgozas/AdatFeldolgozas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdatFeldolgozas
+{
+    class Program
+    {
+        struct Szemely
+        {
+            public string vezeteknev;
+            public string keresztnev;
+            public int szuletesEve;
+            public string szuletesiHely;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Szemely> szemelyek = new List<Szemely>();
+            Szemely szemely = new Szemely();
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
